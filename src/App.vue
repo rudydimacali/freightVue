3c46a651353c81792f04238c06d5ef158bd2dbe0
Rudy <rudydimacali@gmail.com>
2019-03-06 13:52:23 -0800

    Initialize Vue app.

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <OrderForm/>
+  </div>
+</template>
+<script>
+
+import OrderForm from './components/OrderForm.vue';
+
+export default {
+  name: 'App',
+  components: {
+    OrderForm,
+  },
+};
+
+</script>
+<style>
+#app {
+  font-family: "myriad-pro", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #13a7a0;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/OrderForm.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="orderForm">
+    <h1>Freight Train Order Form</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'OrderForm',
+  data() {
+    return {
+      wagons: [],
+    };
+  },
+  methods: {
+  },
+};
+
+
+</script>
+
+<style scoped>
+</style>
