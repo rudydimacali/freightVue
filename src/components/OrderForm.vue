070f839c32358dbe40d1c421ff6ae1c16b5d3db6
Rudy <rudydimacali@gmail.com>
2019-03-06 14:18:26 -0800

    Add conditional rendering for wagon type options.

--- src/components/OrderForm.vue
+++ src/components/OrderForm.vue
@@ -1,6 +1,17 @@
 <template>
-  <div class="orderForm">
+  <div id="OrderFormContainer">
     <h1>Freight Train Order Form</h1>
+    <form id="OrderForm">
+      <select v-model="parentWagonType" @change="subWagonType = parentWagonType">
+        <option v-for="(type, index) in wagonTypes" :value="type" :key="index">{{ type }}</option>
+      </select>
+      <select v-model="subWagonType" v-if="parentWagonType === 'Covered'">
+        <option v-for="(type, index) in coveredTypes" :value="type" :key="index">{{ type }}</option>
+      </select>
+      <select v-model="subWagonType" v-if="parentWagonType === 'Tank'">
+        <option v-for="(type, index) in tankTypes" :value="type" :key="index">{{ type }}</option>
+      </select>
+    </form>
   </div>
 </template>
 
@@ -10,6 +21,11 @@ export default {
   data() {
     return {
       wagons: [],
+      wagonTypes: ['Open', 'Covered', 'Flat', 'Tank'],
+      coveredTypes: ['Regular', 'Refrigerated', 'Livestock'],
+      tankTypes: ['Liquid', 'Gas', 'Refrigerated Liquid', 'Refrigerated Gas'],
+      parentWagonType: 'Open',
+      subWagonType: 'Open',
     };
   },
   methods: {
